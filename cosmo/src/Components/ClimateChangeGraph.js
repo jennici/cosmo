c401aebaae294b97b9c50506b2386d34252161a8
jennici <jennifer.konopka@hotmail.de>
2019-11-27 11:29:41 +0100

    created climate graph component

---
+++ cosmo/src/Components/ClimateChangeGraph.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ClimateChangeGraph extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
