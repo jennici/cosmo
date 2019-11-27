9b571415d69b34e61007491bad0c1e4917843df5
jennici <jennifer.konopka@hotmail.de>
2019-11-27 16:51:49 +0100

    set up fetching api

--- cosmo/src/App.js
+++ cosmo/src/App.js
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import './semantic-ui-css.min';
 import './bootstrap.min';
 
-function App() {
+export default class App extends Component {
+
+  state = {
+    emission: [], //How do we want to name our main object in the state?
+    loading: true
+  }
+
+  async componentDidMount() {
+    const url = "https://my.api.mockaroo.com/co2.json?key=8eb9e6f0";
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({ 
+        emission: data.results, 
+        loading: false 
+    });
+  } 
+
+  render(){
+
+  
   return (
     <div className="App">
   
     </div>
-  );
+    );
+  }
 }
-
-export default App;
