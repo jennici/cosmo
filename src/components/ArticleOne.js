d1d819ee26b98c3791a6977dd9a09f4f1b74367b
Nina Nadolski <ninanadolski@Ninas-MacBook-Air.local>
2019-12-13 13:37:17 +0100

    import react, dom and containers

---
+++ src/components/ArticleOne.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import ReactDOM from 'react-dom';
+import semantic from 'semantic-ui';
+
+
+
+// for an image can take up the width of its container, use: class="ui fluid image"
+<div>
+    <img src="/images/wireframe/image.png" class="ui huge image" />  
+</div>
+
+// Text container with header
+
+<div class="ui text container">
+  <h2 class="ui header">Header</h2>
+  <p>
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+    Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
+    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+    consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+    In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link
+    mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate
+    eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam
+    lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius
+    laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+    ultricies nisi.
+  </p>
+  <p>
+    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor.
+    Aenean massa strong. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur
+    ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+    consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu.
+    In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede link
+    mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate
+    eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam
+    lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius
+    laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper
+    ultricies nisi.
+  </p>
+</div>
