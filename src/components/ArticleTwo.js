77fb12d23a913a3dd2e40aa4ad667d1dbb2cbc7c
Nina Nadolski <ninanadolski@Ninas-MacBook-Air.local>
2019-12-16 17:25:54 +0100

    set up basics for new compentent articleTwo

---
+++ src/components/ArticleTwo.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+
+
+
+
+export default class ArticleOne extends Component {
+    state = {
+  
+    }
+  
+  render() {
+  
+      return (
+          <React.Fragment>
+
+          </React.Fragment>
+      ); 
+   }
+}
