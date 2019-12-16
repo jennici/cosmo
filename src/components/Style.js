76cc855825934149d17f6e38180f0131c6759599
Sannatilda <55459927+Sannatilda@users.noreply.github.com>
2019-12-16 09:27:56 +0100

    added css

--- src/components/Style.js
+++ src/components/Style.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react'
 import style from '../pictures/style.png'
+import styletext from '../pictures/styletext.png'
+import '../css/cosmo.css'
 
 
 class Style extends Component{
@@ -7,6 +9,8 @@ class Style extends Component{
         return(
             <div>
                 <img src={style} style={{width: "100%"}} alt="stylepicture" />
+                <h1>45 Cute Gift Ideas for Any Women In Your Life</h1>
+                <img src={styletext} style={{width: "100%"}} alt="styletextpic" />
             </div>
         )
     }
