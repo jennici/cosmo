ba9e9513956723dbbaf2232b14c7b39e6c5fef79
Nina Nadolski <ninanadolski@Ninas-MacBook-Air.local>
2019-12-16 13:16:47 +0100

    set up share button component

--- src/components/ArticleOne.js
+++ src/components/ArticleOne.js
@@ -23,21 +23,23 @@ render() {
         </div>
 
         <div>
-            <img src={picmakeup} className="ui huge image" alt="huge" style={{margin: 'auto', padding: '3%'}}/>
+            <img src={picmakeup} className="ui huge image" alt="huge" style={{margin:'auto', padding:'2%'}}/>
         </div>
 
-        <button className="ui circular facebook icon button">
-            <i className="facebook icon"></i>
-        </button>
-        <button className="ui circular twitter icon button">
-            <i className="twitter icon"></i>
-        </button>
-        <button className="ui circular linkedin icon button">
-            <i className="linkedin icon"></i>
-        </button>
-        <button className="ui circular instagram icon button">
-            <i className="instagram icon"></i>
-        </button>
+       {/* <div style={{padding:'2px'}}>
+            <button className="ui circular facebook icon button">
+                <i className="facebook icon"></i>
+            </button>
+            <button className="ui circular twitter icon button">
+                <i className="twitter icon"></i>
+            </button>
+            <button className="ui circular linkedin icon button">
+                <i className="linkedin icon"></i>
+            </button>
+            <button className="ui circular instagram icon button">
+                <i className="instagram icon"></i>
+            </button>
+           </div> */}
 
         <div className="ui text container"> 
             <p>
