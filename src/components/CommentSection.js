b04f48dfacdbe1d63cff9aa2e194ab6d57c80adb
Nina Nadolski <ninanadolski@Ninas-MacBook-Air.local>
2019-12-16 08:47:01 +0100

    fixed } problem

--- src/components/CommentSection.js
+++ src/components/CommentSection.js
@@ -1,2 +1,11 @@
 import React, { Component } from "react";
-import ReactDOM from 'react-dom';
+import semantic from 'semantic-ui'; // whats wrong here?
+
+export default class CommentSection extends Component {
+    state = {
+        loading: true,
+    }
+
+
+
+    }
