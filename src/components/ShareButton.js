10676e587e435d3932c0d0e6f5a416576ae52d7d
Nina Nadolski <ninanadolski@Ninas-MacBook-Air.local>
2019-12-16 13:16:59 +0100

    set up the share button component

--- src/components/ShareButton.js
+++ src/components/ShareButton.js
@@ -1,2 +1,26 @@
 import React, { Component } from "react";
-import ReactDOM from 'react-dom';
+
+export default class ShareButton extends Component {
+    state = {
+    }
+  
+  render() {
+  
+      return (
+<div style={{padding:'2px'}}>
+            <button className="ui circular facebook icon button">
+                <i className="facebook icon"></i>
+            </button>
+            <button className="ui circular twitter icon button">
+                <i className="twitter icon"></i>
+            </button>
+            <button className="ui circular linkedin icon button">
+                <i className="linkedin icon"></i>
+            </button>
+            <button className="ui circular instagram icon button">
+                <i className="instagram icon"></i>
+            </button>
+           </div>
+      )
+  }
+}
