1a1a1658523b4d1c4eacb473c3850c0085a2c806
Nina Nadolski <ninanadolski@Ninas-MacBook-Air.local>
2019-12-16 23:48:12 +0100

    put in pictures and headers

--- src/components/ArticleTwo.js
+++ src/components/ArticleTwo.js
@@ -6,6 +6,9 @@ import picarrow from '../pictures/picarrow.svg';
 import picavatar2 from '../pictures/picavatar2.png';
 import picskincare2 from '../pictures/picskincare2.png';
 import picbio from '../pictures/picbio.png';
+import picethical from '../pictures/picethical.png';
+import picfair from '../pictures/picfair.png';
+import picvegan from '../pictures/picvegan.png';
 
 export default class ArticleTwo extends Component {
     state = {
@@ -43,16 +46,65 @@ export default class ArticleTwo extends Component {
             define all the words you need in your vocab. Your friends will be *so* impressed next time you go post-brunch shopping.
             </p>
 
+            {/* BIODEGRADABLE */}
+
             <h2 style={{fontFamily:'Futura, Helvetica', fontSize:'30px', fontWeight:'800', letterSpacing:'2.55px', color:'#ff4c49', backgroundColor:'#fff5f2', padding:'9%'}}> “Biode­gradable” </h2>
 
-            <p>
+            <p style={{padding:'2%'}}>
+            This stuff is made with minimal damage to the planet—aka without polluting all over the place.
+            </p>
+
+            <a href="https://www.vaude.com/en-GB/Products/Eco-Fair-Collection/Sustainable-Materials/Primaloft/" target="_blank" rel="noopener noreferrer" className="ui huge image">
+                <img src={picbio} alt="huge" style={{margin:'auto', padding:'2%'}} />
+            </a>
+
+            {/* ECO-FRIENDLY */}
+            
+            <h2 style={{fontFamily:'Futura, Helvetica', fontSize:'30px', fontWeight:'800', letterSpacing:'2.55px', color:'#ff4c49', backgroundColor:'#fff5f2', padding:'9%'}}> “Eco-friendly” </h2>
+
+            <p style={{padding:'2%'}}>
             Silk, linen, wool, and hemp ­fabrics easily decompose into natural ­elements 
             (as in, not landfill filler) after being trashed.
             </p>
 
-            <div>
-                <img src={picbio} className="ui huge image" alt="huge" style={{margin:'auto', padding:'2%'}}/>
-            </div>
+            {/* ETHICAL */}
+
+            <h2 style={{fontFamily:'Futura, Helvetica', fontSize:'30px', fontWeight:'800', letterSpacing:'2.55px', color:'#ff4c49', backgroundColor:'#fff5f2', padding:'9%'}}> “Ethical” </h2>
+
+            <p style={{padding:'2%'}}>
+            An umbrella term that essentially means the people who worked on an item were treated safely and paid fairly. 
+            Buying ­ethical means you’ll empower others—and get all the compliments.
+            </p>
+
+            <a href="https://www.outdoorvoices.com/products/techsweat-7-8-zoom-legging?variant=21464490180686" target="_blank" rel="noopener noreferrer" className="ui huge image">
+                <img src={picethical} alt="huge" style={{margin:'auto', padding:'2%'}} />
+            </a>
+
+            {/* FAIR TRADE */}
+
+            <h2 style={{fontFamily:'Futura, Helvetica', fontSize:'30px', fontWeight:'800', letterSpacing:'2.55px', color:'#ff4c49', backgroundColor:'#fff5f2', padding:'9%'}}> “Fair trade” </h2>
+
+            <p style={{padding:'2%'}}>
+            If your v cute retro denim minidress is fair trade, 
+            it was made by workers in an underdeveloped country who earn a fair living wage.
+            </p>
+
+            <a href="https://www.outdoorvoices.com/products/techsweat-7-8-zoom-legging?variant=21464490180686" target="_blank" rel="noopener noreferrer" className="ui huge image">
+                <img src={picfair} alt="huge" style={{margin:'auto', padding:'2%'}} />
+            </a>
+
+            {/* VEGAN */}
+
+            <h2 style={{fontFamily:'Futura, Helvetica', fontSize:'30px', fontWeight:'800', letterSpacing:'2.55px', color:'#ff4c49', backgroundColor:'#fff5f2', padding:'9%'}}> “Vegan” </h2>
+
+            <p style={{padding:'2%'}}>
+            When zero animal products (like leather, fur, or even silk) go into making something. 
+            These gorge python slingbacks, for instance? Not actually from an actual python.
+            </p>
+
+            <a href="https://www.outdoorvoices.com/products/techsweat-7-8-zoom-legging?variant=21464490180686" target="_blank" rel="noopener noreferrer" className="ui huge image">
+                <img src={picvegan} alt="huge" style={{margin:'auto', padding:'2%'}} />
+            </a>
 
         </div>
 
