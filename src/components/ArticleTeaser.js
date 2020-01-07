80579c0a3ab45924a63d2b0713182c73eb662e72
Nasrin Miro <nasrin.miro@gmail.com>
2020-01-07 17:54:32 +0100

    add missing picture files

--- src/components/ArticleTeaser.js
+++ src/components/ArticleTeaser.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react'
 import Column from './Column'
+import beauty1 from '../pictures/beauty.pic1.jpg'
+import beauty2 from '../pictures/beauty.pic2.jpg'
+import interview1 from '../pictures/interview.pic1.jpg'
+import fashion1 from '../pictures/fashion.pic1.jpg'
 
 export default class ArticleTeaser extends Component {
 
@@ -16,29 +20,36 @@ export default class ArticleTeaser extends Component {
           title: 'Cruelty-Free & Vegan Hair Product Brands',
           author: 'SOPHIA',
           url: 'https://ethicalelephant.com/cruelty-free-vegan-hair-products/',
-          image: 'https://ethicalelephant.com/wp-content/uploads/2016/08/cruelty-free-vegan-hair-brands.jpg'
+          image: beauty1
         },
         {
           type: 'Beauty',
           title: 'Nail Polish for Vegans',
           author: 'CARLI CARDELLINO',
           url: 'https://www.cosmopolitan.com/style-beauty/beauty/how-to/a10886/obsessive-compulsivecosmetics-vpl-nail-polish/',
-          image: 'https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a0903aa8f0b_-_cos-obsessive-compulsive-cosmetics-vpl-mdn.jpg?resize=768:*'
+          image: beauty2
+        },
+        {
+          type: 'Beauty',
+          title: 'Nail Polish for Vegans',
+          author: 'CARLI CARDELLINO',
+          url: 'https://www.cosmopolitan.com/style-beauty/beauty/how-to/a10886/obsessive-compulsivecosmetics-vpl-nail-polish/',
+          image: beauty2
         },
         {
           type: 'Fashion',
           title: '21 Sustainable Clothing Brands for People Who Give a Sh*t About the Environment',
           author: 'CARLI CARDELLINO',
           url: 'https://www.cosmopolitan.com/stylebeauty/fashion/g9255699/sustainable-eco-green-clothingbrands/',
-          image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sustainable-fashion-1559169790.jpg?crop=1.00xw:1.00xh;0,0&resize=980:*'
+          image: fashion1
         },
         {
           type: 'Interviews',
           title: 'Interview: Michael Bloomberg on climate change optimism',
           author: 'OZEANA',
           url: 'https://www.theoryculturesociety.org/interview-with-mike-hulme-on-climate-change-and-consumption/',
-          image: 'https://oceana.org/sites/default/files/styles/ntsc/public/subway_1.jpg?itok=Czoi4kvU'
-        }
+          image: interview1
+        },
       ]
     }
   }
