a801f48352f1172b9dcc7510c7f7e3a00714c606
Nasrin Miro <nasrin.miro@gmail.com>
2020-01-07 16:51:36 +0100

    add complete article teaser

--- src/App.js
+++ src/App.js
@@ -1,88 +1,57 @@
-import React, {Component, Image} from 'react';
-import './App.css';
-import 'semantic-ui-css/semantic.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import ArticleTeaser from "./components/ArticleTeaser";
+import React, { Component } from 'react'
+import './App.css'
+import 'semantic-ui-css/semantic.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import ArticleTeaser from './components/ArticleTeaser'
 
 export default class App extends Component {
-
-  state = {
-    emission: [],
-    temperature: [],
-    glaciersize: [],
-    sealevel: [],
-    loading: true,
-    data: [
-      {
-        type: "beauty",
-        title: "Cruelty-Free & Vegan Hair Product Brands",
-        author: "SOPHIA",
-        url: "https://ethicalelephant.com/cruelty-free-vegan-hair-products/",
-        image: "https://ethicalelephant.com/wp-content/uploads/2016/08/cruelty-free-vegan-hair-brands.jpg"
-      },
-      {
-        type: "beauty",
-        title: "Nail Polish for Vegans",
-        author: "CARLI CARDELLINO",
-        url: "https://www.cosmopolitan.com/style-beauty/beauty/how-to/a10886/obsessive-compulsivecosmetics-vpl-nail-polish/",
-        image: "https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a0903aa8f0b_-_cos-obsessive-compulsive-cosmetics-vpl-mdn.jpg?resize=768:*"
-      },
-      {
-        type: "fashion",
-        title: "21 Sustainable Clothing Brands for People Who Give a Sh*t About the Environment",
-        author: "CARLI CARDELLINO",
-        url: "https://www.cosmopolitan.com/stylebeauty/fashion/g9255699/sustainable-eco-green-clothingbrands/",
-        image: "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sustainable-fashion-1559169790.jpg?crop=1.00xw:1.00xh;0,0&resize=980:*"
-      },
-      {
-        type: "interview",
-        title: "Interview: Michael Bloomberg on climate change optimism",
-        author: "OZEANA",
-        url: "https://www.theoryculturesociety.org/interview-with-mike-hulme-on-climate-change-and-consumption/",
-        image: "https://oceana.org/sites/default/files/styles/ntsc/public/subway_1.jpg?itok=Czoi4kvU"
-      },
-    ],
+  constructor (props) {
+    super(props)
+    this.state = {
+      emission: [],
+      temperature: [],
+      glaciersize: [],
+      sealevel: [],
+      loading: true,
+    }
   }
 
-  async componentDidMount() {
-    const url1 = "https://my.api.mockaroo.com/co2.json?key=8eb9e6f0";
-    const url2 = "https://my.api.mockaroo.com/temp.json?key=8eb9e6f0";
-    const url3 = "https://my.api.mockaroo.com/glaciersize.json?key=8eb9e6f0";
-    const url4 = "https://my.api.mockaroo.com/sealevel.json?key=8eb9e6f0";
+  async componentDidMount () {
+    const url1 = 'https://my.api.mockaroo.com/co2.json?key=8eb9e6f0'
+    const url2 = 'https://my.api.mockaroo.com/temp.json?key=8eb9e6f0'
+    const url3 = 'https://my.api.mockaroo.com/glaciersize.json?key=8eb9e6f0'
+    const url4 = 'https://my.api.mockaroo.com/sealevel.json?key=8eb9e6f0'
 
-    const response1 = await fetch(url1);
-    const data1 = await response1.json();
+    const response1 = await fetch(url1)
+    const data1 = await response1.json()
 
-    const response2 = await fetch(url2);
-    const data2 = await response2.json();
+    const response2 = await fetch(url2)
+    const data2 = await response2.json()
 
-    const response3 = await fetch(url3);
-    const data3 = await response3.json();
+    const response3 = await fetch(url3)
+    const data3 = await response3.json()
 
-    const response4 = await fetch(url4);
-    const data4 = await response4.json();
+    const response4 = await fetch(url4)
+    const data4 = await response4.json()
     this.setState({
-        emission: data1,
-        temperature: data2,
-        glaciersize: data3,
-        sealevel: data4,
-        loading: false
-    });
+      emission: data1,
+      temperature: data2,
+      glaciersize: data3,
+      sealevel: data4,
+      loading: false
+    })
 
-    console.log(this.state.emission);
-    console.log(this.state.temperature);
-    console.log(this.state.glaciersize);
-    console.log(this.state.sealevel);
+    console.log(this.state.emission)
+    console.log(this.state.temperature)
+    console.log(this.state.glaciersize)
+    console.log(this.state.sealevel)
   }
 
-
-
-  render(){
-    
+  render () {
     return (
       <div>
-        <ArticleTeaser />
-      </div>
-    );
+      <ArticleTeaser />
+    </div>
+    )
   }
 }
--- src/components/ArticleTeaser.js
+++ src/components/ArticleTeaser.js
@@ -1,32 +1,55 @@
 import React, { Component } from 'react'
+import Column from './Column'
 
 export default class ArticleTeaser extends Component {
-    render() {
-        
-        const types = ["beauty", "fashion", "interview"];
 
-        return (
-            <div>
-                <div style={{ "display": "flex" }}>
-                    {types.map(type => {
-                    return (
-                        <div style={{ "display": "flex", "flexDirection": "column", "margin": "50px" }}>
-                            <h1>{type}</h1>
-                                {this.state.data.filter(item => item.type === type).map(item => {
-                                return (
-                                    <a style={{ "display": "flex" }} href={item.url} className="App">
-                                        <img style={{ "max-height": "200px", "max-width": "200px" }} src={item.image}></img>
-                                        <div>
-                                            <p>{item.title}</p>
-                                            <p>-->{item.author}</p>
-                                        </div>
-                                    </a>
-                                    )
-                                })}
-                            </div>)}
-                        )}
-                </div>
-            </div>
-        )
+  constructor (props) {
+    super(props)
+    this.state = {
+      /*
+        Hier werden die Daten für die Artikel statisch gespeichert.
+        Diese können durch weitere Einträge erweitert werden
+      */
+      data: [
+        {
+          type: 'Beauty',
+          title: 'Cruelty-Free & Vegan Hair Product Brands',
+          author: 'SOPHIA',
+          url: 'https://ethicalelephant.com/cruelty-free-vegan-hair-products/',
+          image: 'https://ethicalelephant.com/wp-content/uploads/2016/08/cruelty-free-vegan-hair-brands.jpg'
+        },
+        {
+          type: 'Beauty',
+          title: 'Nail Polish for Vegans',
+          author: 'CARLI CARDELLINO',
+          url: 'https://www.cosmopolitan.com/style-beauty/beauty/how-to/a10886/obsessive-compulsivecosmetics-vpl-nail-polish/',
+          image: 'https://hips.hearstapps.com/cos.h-cdn.co/assets/cm/14/25/53a0903aa8f0b_-_cos-obsessive-compulsive-cosmetics-vpl-mdn.jpg?resize=768:*'
+        },
+        {
+          type: 'Fashion',
+          title: '21 Sustainable Clothing Brands for People Who Give a Sh*t About the Environment',
+          author: 'CARLI CARDELLINO',
+          url: 'https://www.cosmopolitan.com/stylebeauty/fashion/g9255699/sustainable-eco-green-clothingbrands/',
+          image: 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/sustainable-fashion-1559169790.jpg?crop=1.00xw:1.00xh;0,0&resize=980:*'
+        },
+        {
+          type: 'Interviews',
+          title: 'Interview: Michael Bloomberg on climate change optimism',
+          author: 'OZEANA',
+          url: 'https://www.theoryculturesociety.org/interview-with-mike-hulme-on-climate-change-and-consumption/',
+          image: 'https://oceana.org/sites/default/files/styles/ntsc/public/subway_1.jpg?itok=Czoi4kvU'
+        }
+      ]
     }
+  }
+    render () {
+        // Aufteilung der Artikel in benannte Kategorien
+        const types = ['Beauty', 'Fashion', 'Interviews']
+        return (
+          <div style={{ display: 'flex', background: 'antiquewhite' }}>
+            {/* Jede Kategorie bekommt eine Spalte */}
+            {types.map(type => <Column type={type} state={this.state}></Column>)}
+          </div>
+        )
+      }
 }
