0417b5b8f29f29f5dde521809d7bc82994315d2d
Sannatilda <55459927+Sannatilda@users.noreply.github.com>
2020-01-08 16:14:59 +0100

    improved article teasers

--- src/components/ArticleT.js
+++ src/components/ArticleT.js
@@ -1,42 +1,40 @@
 import React, {Component} from 'react'
 import picmakeup from '../pictures/picmakeup.png'
-import picskincare2 from '../pictures/picskincare2.png'
 
 class ArticleT extends Component{
     render(){
         return(
             <div className="teaser">
                 <div className="ui grid">
-                <div className="five wide column">beauty</div>
-                <div className="five wide column">fashion</div>
-                <div className="five wide column">interviews</div>
+                <div className="five wide column">beauty
+                <div className="article-card">
+                    <img className="article-pic" style={{float:"left"}} src={picmakeup} alt="makeup article"/>
+                    <p className="article-text">5 vegan makeup brands you need to know about</p>
                 </div>
-
-                
-                <div className="ui grid">
-                <div className="two wide column">
-                    <img className="article-pic" src={picmakeup} alt="makeup article"/>
-                </div>
-                <div className="two wide column">
-                    <p>5 vegan makeup brands you need to know about</p>
+                <div className="article-card">
+                    <img className="article-pic" style={{float:"left"}}src={picmakeup} alt="makeup article"/>
+                    <p className="article-text">5 vegan makeup brands you need to know about</p>
                 </div>
                 </div>
-
-                <div className="ui grid">
-                <div className="two wide column">
-                    <img className="article-pic" src={picskincare2} alt="skincare artickle"/>
+                <div className="five wide column">fashion
+                <div className="article-card">
+                    <img className="article-pic" style={{float:"left"}} src={picmakeup} alt="makeup article"/>
+                    <p className="article-text">5 vegan makeup brands you need to know about</p>
                 </div>
-                <div className="two wide column">
-                    <p>Okay, but What Does “Sustainable Fashion” Actually Mean?</p>
+                <div className="article-card">
+                    <img className="article-pic" style={{float:"left"}}src={picmakeup} alt="makeup article"/>
+                    <p className="article-text">5 vegan makeup brands you need to know about</p>
                 </div>
                 </div>
-
-                <div className="ui grid">
-                <div className="two wide column">
-                    <img className="article-pic" src={picskincare2} alt="skincare artickle"/>
+                <div className="five wide column">interviews
+                <div className="article-card">
+                    <img className="article-pic" style={{float:"left"}} src={picmakeup} alt="makeup article"/>
+                    <p className="article-text">5 vegan makeup brands you need to know about</p>
                 </div>
-                <div className="two wide column">
-                    <p>Okay, but What Does “Sustainable Fashion” Actually Mean?</p>
+                <div className="article-card">
+                    <img className="article-pic" style={{float:"left"}}src={picmakeup} alt="makeup article"/>
+                    <p className="article-text">5 vegan makeup brands you need to know about</p>
+                </div>
                 </div>
                 </div>
             </div>
